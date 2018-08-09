da11dcba50349df8d8af2e5b9a04df0bd4eaf605
Helio Lima <helio.augustolima@gmail.com>
2018-08-08 22:38:11 -0300

    Default handle of errors with Alert

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -14,7 +14,8 @@ export class ClienteService {
 
     findByEmail(email: string) : Observable<ClienteDTO>{
 
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`);
+        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`
+        );
     }
 
     getImageFromBucket(id: string) : Observable<any> {

282ed93359d16137dba902be22f85c2dec508d35
Helio Lima <helio.augustolima@gmail.com>
2018-08-12 15:56:07 -0300

    Loading products by category

---
+++ src/services/domain/produto.servise.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "../../../node_modules/@angular/core";
+import { HttpClient } from "../../../node_modules/@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { Observable } from "rxjs/Rx";
+import { ProdutoDTO } from "../../models/produto.dto";
+
+@Injectable()
+export class ProdutoService {
+
+    constructor(public http: HttpClient){
+
+    }
+
+    findByCategoria(categoria_id : string) : Observable<ProdutoDTO[]> {
+        return this.http.get<ProdutoDTO[]>(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+}

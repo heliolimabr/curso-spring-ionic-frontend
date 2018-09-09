a7b1329f935f6f35b02680b822d34e072fc388bf
Helio Lima <helio.augustolima@gmail.com>
2018-09-08 22:44:21 -0300

    Using cordova to take a picture with the camera

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,52 +1,76 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { StorageService } from '../../services/storage.service';
-import { ClienteDTO } from '../../models/cliente.dto';
-import { ClienteService } from '../../services/domain/cliente.service';
-import { API_CONFIG } from '../../config/api.config';
-
-
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { StorageService } from "../../services/storage.service";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { ClienteService } from "../../services/domain/cliente.service";
+import { API_CONFIG } from "../../config/api.config";
+import { CameraOptions, Camera } from "@ionic-native/camera";
 
 @IonicPage()
 @Component({
-  selector: 'page-profile',
-  templateUrl: 'profile.html',
+  selector: "page-profile",
+  templateUrl: "profile.html"
 })
 export class ProfilePage {
-
-  cliente : ClienteDTO;
+  cliente: ClienteDTO;
+  picture: string;
+  cameraOn: boolean = false;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
-    public clienteService: ClienteService) {
-  }
+    public clienteService: ClienteService,
+    public camera: Camera
+  ) {}
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
-    if(localUser && localUser.email){
-      this.clienteService.findByEmail(localUser.email)
-        .subscribe(response => {
+    if (localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email).subscribe(
+        response => {
           this.cliente = response as ClienteDTO;
           this.getImageIfExists();
         },
         error => {
-          if(error.status == 403){
-            this.navCtrl.setRoot('HomePage');
+          if (error.status == 403) {
+            this.navCtrl.setRoot("HomePage");
           }
-        });
-    }else{
-      this.navCtrl.setRoot('HomePage');
+        }
+      );
+    } else {
+      this.navCtrl.setRoot("HomePage");
     }
   }
 
-  getImageIfExists(){
-    this.clienteService.getImageFromBucket(this.cliente.id)
-      .subscribe(response => {
-        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id).subscribe(
+      response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${
+          this.cliente.id
+        }.jpg`;
       },
-      error => {});
+      error => {}
+    );
   }
 
+  getCameraPicture() {
+    this.cameraOn = true;
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE
+    };
+
+    this.camera.getPicture(options).then(
+      imageData => {
+        this.picture = "data:image/png;base64," + imageData;
+        this.cameraOn = false;
+      },
+      err => {
+        // Handle error
+      }
+    );
+  }
 }
